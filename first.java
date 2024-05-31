1f11492fd463efe4893fbc598f36a83a00c87976
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-05-31 12:47:47 +0530

    inheritance examples

--- first.java
+++ first.java
@@ -1,7 +1,15 @@
 
 public class first {
     public static void main(String[] args) {
+        engineer e1=new engineer();
+        System.out.println("benfits are"+e1.benfits);
         
     }
     
+}
+class employee{
+    int salary=60000;
+}
+class engineer extends employee{
+    int benfits=10000;
 }
