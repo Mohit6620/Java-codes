904a50e001d60888d8eebcdcab68684d02037be6
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-02 16:33:23 +0530

    classes and object

--- second.java
+++ second.java
@@ -10,9 +10,15 @@ public class second {
         student sc=new student();
         sc.details(1,"ram" , "mca");
         sc.average(76,83,59);
+
+        //data hiding
+        hiding h1=new hiding();
+        h1.setone(40); //setting one value
+        h1.settwo(50);//setting two value 
+        h1.add();
+
     
-    }
-    
+    }    
 }
 class bank {
     int total_balance=4000;
@@ -22,11 +28,8 @@ class bank {
          int total=total_balance+credit_amount;
         System.out.println("your total balance  after credit is  "+total);
 
-    }
-
-    
+    }    
 }
-
 class rectangle {
     double length;
     double breadth;
@@ -42,7 +45,6 @@ class rectangle {
         return y;
     }
 }
-
 class student {
         int roll;
         String name;
@@ -61,5 +63,29 @@ class student {
 
         return avg;
     }
-}
+}    
+//Data hiding - means allow access only those date on which user needed 
+class hiding {
+    private int one;
+    private int two;
     
+    //just another way to passing the date in the method 
+    int getone (){  //this method allows us to read the property 
+        return one;
+    }
+    void setone (int o){ //this set method allows us to write the property   
+    one =o;  // when we use this two methods with the same properties then we can read and write the given value 
+    }
+    int gettwo (){
+        return two;
+    }
+    void settwo (int t){
+    two=t;
+    }
+    int add(){
+        int cal=one+two;
+        System.out.println(cal);
+        return one+two;
+    }
+} 
+// CONSTRUCTOR 
