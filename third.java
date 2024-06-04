e580477e71ea29aa7a3f5e4d7e83be85a2441d03
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-04 19:59:51 +0530

    all types of inheritance

--- third.java
+++ third.java
@@ -1,3 +1,23 @@
+//types of inheritance
+// Single Inheritance
+// Multilevel Inheritance
+// Hierarchical Inheritance
+// Multiple Inheritance
+//Hybrid Inheritance
 public class third {
-    
+    public static void main(String[] args) {
+        apple a1=new apple();
+        System.out.println("Calling fruit class into apple"a1.single());
+
+    }
+}
+class fruit {
+    void original (){
+        System.out.println("this is the first fruit class ");
+    }
+}
+class apple extends fruit  {
+    void single (){
+        System.out.println("this is single level inheritance");
+    }
 }
