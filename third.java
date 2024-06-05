e3222b2911eb5c85b2e1361999442ab6e615c8e4
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-05 21:28:27 +0530

    all types of inheritance

--- third.java
+++ third.java
@@ -21,3 +21,17 @@ class apple extends fruit  {
         System.out.println("this is single level inheritance");
     }
 }
+class kashmir_apple extends apple {
+    void multi (){
+        System.out.println("this is multi level  inheritance");// also example of hiearchical level 
+
+    }
+}
+class shimla_apple extends apple {
+    void multi (){
+        System.out.println("this is Hierarchical level  inheritance"); // two or more class extending from  a single class
+
+    }
+}
+// in multiple inheritance a single class is extended by two or more classes
+// in hybrid inheritance "A" class is extended by another  "B" class and "B" is extended by "C"and "C" is extended by "A" 
